ab67e7c0693bf4e0855557c6b2f1d2fe6fc8ac98
rkddusen <k99endus@naver.com>
2026-02-12 17:24:00 +0900

    :sparkles: [#3] 구글 로그인 ui 및 api 호출 생성

---
+++ src/app/api/oauth/google/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const { GOOGLE_CLIENT_ID, NEXT_PUBLIC_GOOGLE_REDIRECT_URI } = process.env;
+
+  if (!GOOGLE_CLIENT_ID || !NEXT_PUBLIC_GOOGLE_REDIRECT_URI) {
+    return NextResponse.json(
+      { message: 'Google OAuth Environment variables are missing' },
+      { status: 500 }
+    );
+  }
+
+  const baseUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+  const params = new URLSearchParams({
+    client_id: GOOGLE_CLIENT_ID,
+    redirect_uri: NEXT_PUBLIC_GOOGLE_REDIRECT_URI,
+    response_type: 'code',
+    scope: 'email profile openid',
+    access_type: 'offline', // Refresh Token을 받기 위해 필요할 수 있음
+    prompt: 'consent', // 항상 동의 화면 노출 (선택사항)
+  });
+
+  const googleAuthUrl = `${baseUrl}?${params.toString()}`;
+
+  return NextResponse.redirect(googleAuthUrl);
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,11 @@ import { LoginForm } from '@/features/auth/ui/login-form';
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50">
-      <LoginForm />
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50/50 p-4 relative overflow-hidden">
+    <div className="absolute top-0 left-0 right-0 h-96 bg-gradient-to-b from-blue-50/50 to-transparent blur-3xl -z-10" />
+      <main className="relative z-10 w-full max-w-md">
+        <LoginForm />
+      </main>
     </div>
   );
 }
