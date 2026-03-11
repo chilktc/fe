d6dbc42c54145569cdc7f4b3ecb0324e60a2fa96
rkddusen <k99endus@naver.com>
2026-03-11 15:45:42 +0900

    :sparkles: [#7] 개인 정보 수정 구현

--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,4 +1,3 @@
-import { Profile } from "@/entities/user/ui";
 import { User } from "@/entities/user/model/types";
 import Link from "next/link";
 import { SIDEBAR_MENU_ITEMS } from "../model/menu";
@@ -23,32 +22,29 @@ export function Sidebar({ isOpen, onClose, user }: SidebarProps) {
 
       {/* Sidebar Content */}
       <aside
-        className={`absolute top-0 left-0 h-dvh w-[285px] py-4 bg-gray-200 shadow-2xl z-50 overflow-y-auto transform transition-transform duration-300 ease-in-out border-r border-gray-400 ${
+        className={`absolute top-0 left-0 h-dvh w-[285px] max-w-[90%] py-4 bg-gray-200 shadow-2xl z-50 overflow-y-auto transform transition-transform duration-300 ease-in-out border-r border-gray-400 ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
         <div className="flex flex-col h-full">
-          <div className="px-7.5 pb-4">
+          <div className="px-7.5 py-4">
             <div className="flex items-center space-x-6">
-              <Profile
-                src={user.profileImage}
-                nickname={user.nickname}
-                size={64}
-              />
               <div className="flex-1">
-                <p className="text-label-1 text-foreground">{user.nickname}</p>
+                <p className="text-label-1 text-gray-900">{user.nickname}</p>
                 <p className="text-caption-2 text-gray-800">{user.email}</p>
               </div>
             </div>
           </div>
-          <Divide className="bg-gray-100" />
+          <div className="px-4 py-4">
+            <Divide className="bg-gray-400" />
+          </div>
 
           <nav className="flex-1 py-4 space-y-4 overflow-y-auto px-7.5">
             {SIDEBAR_MENU_ITEMS.map((item) => (
               <Link
                 key={item.label}
                 href={item.href}
-                className="flex items-center text-foreground hover:text-primary-600 transition-colors"
+                className="flex items-center text-heading-6 text-gray-900 hover:text-primary-600 transition-colors"
                 onClick={onClose}
               >
                 <span className="text-label-1">{item.label}</span>
