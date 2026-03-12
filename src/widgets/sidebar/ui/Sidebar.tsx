8e929ec9e77062234c90cd05e4327f1f9585a9e0
rkddusen <k99endus@naver.com>
2026-03-12 15:07:54 +0900

    :sparkles: [#7] 로그아웃 구현

--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { User } from "@/entities/user/model/types";
 import Link from "next/link";
 import { SIDEBAR_MENU_ITEMS } from "../model/menu";
-import { Divide } from "@/shared/ui";
+import { Divide, Modal } from "@/shared/ui";
+import { WarningIcon } from "@/shared/icons";
+import { useLogout } from "@/entities/user/api/use-logout";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -10,6 +13,13 @@ interface SidebarProps {
 }
 
 export function Sidebar({ isOpen, onClose, user }: SidebarProps) {
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+  const { mutate: logout, isPending } = useLogout();
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
     <>
       {/* Backdrop */}
@@ -53,12 +63,36 @@ export function Sidebar({ isOpen, onClose, user }: SidebarProps) {
           </nav>
 
           <div className="w-full flex py-2.5 px-7.5">
-            <p className="hover:cursor-pointer text-accent-red text-button-1">
+            <button
+              onClick={() => setIsLogoutModalOpen(true)}
+              className="hover:cursor-pointer text-accent-red text-button-1 outline-none"
+            >
               로그아웃
-            </p>
+            </button>
           </div>
         </div>
       </aside>
+
+      <Modal
+        isOpen={isLogoutModalOpen}
+        onClose={() => setIsLogoutModalOpen(false)}
+        onSubmit={handleLogout}
+        submitLabel="로그아웃"
+        isSubmitLoading={isPending}
+      >
+        <div className="flex flex-col items-center gap-4">
+          <WarningIcon />
+          <div className="space-y-1">
+            <h3 className="text-heading-5 text-gray-900 text-center">
+              정말 로그아웃 하시겠습니까?
+            </h3>
+            <p className="text-body-6 text-gray-800 text-center whitespace-pre-line">
+              로그아웃하면 이 기기에서 검색 기록이{`\n`}삭제됩니다. 동기화된
+              검색 기록은 다시{`\n`}로그인하면 사용할 수 있습니다.
+            </p>
+          </div>
+        </div>
+      </Modal>
     </>
   );
 }
